f46aee4f6600f3f61e22270a5d7d078b5fe31a3d
Qiao Yan Soh <33329143+sohqy@users.noreply.github.com>
2021-07-27 17:27:12 +0800

    Add files via upload

--- LevelSensor1.py
+++ LevelSensor1.py
@@ -4,25 +4,46 @@ UWHS LEVEL SENSOR DATA LOGGING SCRIPT
 =============================
 MaxBotix MB7366, Serial Output. 
 
+All-inclusive script for measuring water level in UWHS tanks. 
+----- INITIALISATION OPTIONS: 
+    - Start time wait
+    - Read interval
+    - Sensor height input (!) 
+
+----- INDICATOR OVERVIEW:
+    - Script initialized: Yellow LED blinks 5 times.
+    - Infinite loop is running: Green LED slow blink.
+    - Data is writing: Red LED on.
+
 Created by Qiao Yan Soh 11 Mar 2021
-Last updated 5 July 2021
+Last updated 27 July 2021
 """
 
+from gpiozero import LED
 import time 
 from serial import Serial
+
+# ========== INDICATOR SET UP 
+Red = LED(26)
+Yellow = LED(19)
+Green = LED(13)
+
+Yellow.blink(on_time = 0.5, off_time = 0.5, n = 5)  # Script initialised
+
+# ========== SENSOR SET UP 
 
 MaxWait = 3     # Number of seconds to wait for reading
 
 def Measure(portName):  
     ser = Serial(portName, 9600, 8, 'N', 1, timeout=1)
-    timeStart = time.time()
+    timeStart = time.time()         # Get current time
     valueCount = 0
 
     while time.time() < timeStart + MaxWait:
-        if ser.inWaiting():
+        if ser.inWaiting():             # if there are things to read
             bytesToRead = ser.inWaiting()
             valueCount += 1
-            if valueCount < 2: # 1st reading may be partial number; throw it out
+            if valueCount < 2:          # 1st reading may be partial number; throw it out
                 continue
             testData = ser.read(bytesToRead)
             if not testData.startswith(b'R'):
@@ -45,13 +66,12 @@ def Measure(portName):
     raise RuntimeError("Expected serial data not received")
 
 def WriteData(CurrentReading):
-    File = open('LevelSensorData.csv', 'a')
+    Red.on()
+    File = open('LevelSensorData.csv', 'a')     # Add name of tank here. 
     File.write(CurrentReading)
     File.close()
+    Red.off()
     
-def SendData(CurrentReading):
-    print('Data Sent' + CurrentReading)
-
 #%% 
 serialDevice = "/dev/ttyAMA0"   # default for RaspberryPi
 
@@ -68,6 +88,7 @@ File.close()
 time.sleep(60 - time.localtime().tm_sec)   # Wait to start on the minute
 
 while True:          
+    Green.blink(on_time = 0.5)
     MeasuredDistance = Measure(serialDevice)
     MeasureTime = time.time()
     if MeasuredDistance >= MaxRange:
@@ -76,7 +97,6 @@ while True:
         Level = SensorHeight - MeasuredDistance
         Reading = str(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(MeasureTime))) + ', ' + str(Level) + '\n'
     WriteData(Reading)
-    SendData(Reading)
     time.sleep(ReadInterval)    # Wait.
 
     
--- RainGauge.py
+++ RainGauge.py
@@ -3,14 +3,33 @@ RAINGAUGE DATA LOGGING SCRIPT
 =============================
 PRONAMIC Rain-O-Matic Pro, PCB 9601, Single reed switch.
 
+All-inclusive script for local data collection of the raingauge. 
+----- INITIALISATION OPTIONS: 
+    - Start time wait
+    - Read interval
+
+----- INDICATOR OVERVIEW:
+    - Script initialized: Yellow LED blinks 5 times.
+    - Infinite loop is running: Green LED slow blink.
+    - Data is writing: Red LED on.
+
 Created by Qiao Yan Soh 11 Mar 2021
-Last updated 5 July 2021
+Last updated 27 July 2021
 
 """
 
 from gpiozero import Button 
+from gpiozero import LED
 import time 
 
+# ----- INDICATOR SET UP
+Red = LED(22)
+Yellow = LED(27)
+Green = LED(17)
+
+Yellow.blink(on_time = 0.5, off_time = 0.5, n = 5)  # Script initialised
+
+# ----- SENSOR SET UP 
 sensor = Button(5) 		# Spoon is connected to RPi GPIO 5 (Pin 29)
 Spoonsize = 0.2			# Manufacturer information 
 count = 0               # Initialize counter
@@ -31,12 +50,11 @@ def ResetCount():
 	count = 0
 	
 def WriteData(CurrentReading):
-	File = open('RainGaugeData.csv', 'a')
+	Red.on()
+    File = open('RainGaugeData.csv', 'a')
 	File.write(CurrentReading)
 	File.close()
-	
-def SendData(CurrentReading):
-	print('Data Sent: ' + CurrentReading)
+	Red.off()
 	
 # ========== 
 sensor.when_pressed = Tip
@@ -44,10 +62,10 @@ sensor.when_pressed = Tip
 time.sleep(60 - time.localtime().tm_sec) # Wait to start on the minute. TO BE CHANGED
 
 while True:         # Run forever   
+    Green.blink(on_time = 0.5)
     ResetCount()
     Rainfall = count * Spoonsize
-    Reading = str(time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time()))) + ', ' + '{:.1f}'.format(Rainfall) + '\n'
+    Reading = str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))) + ', ' + '{:.1f}'.format(Rainfall) + '\n'
     WriteData(Reading)
-    SendData(Reading)
     time.sleep(interval)
 
